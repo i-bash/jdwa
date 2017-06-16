ccb9e2b2e6391834a4d117d7b7ca4cc66f9e801d
Ilya Bashtanov <bashtanov@gmail.com>
2017-06-16 08:31:42 +0300

    day4 last minute changes

--- server/Guess.java
+++ server/Guess.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Guess {
  public static void main(String [] args) throws Exception {
+    System.out.println("Welcome");
     Scanner scan = new Scanner(System.in);
     int num = (int)Math.floor(Math.random()*100.0)+1;
+		Thread.sleep(2000);
     System.out.println("Enter a number");
     while(true){
         String input = scan.nextLine();
@@ -18,6 +20,8 @@ public class Guess {
             System.out.println("Please enter a number between 1 and 100");
         else if(nextInt==num){
             System.out.println("Exactly!");
+						Thread.sleep(3000);
+						System.out.println("Thank you");
             return;
         }
         else System.out.println("Your number is too "+(nextInt<num?"small":"big"));
